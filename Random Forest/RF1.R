082a0674347ccd1bb5320ba83215dfc7033bf3da
shaunak1105 <shaunak1105@gmail.com>
2017-12-26 01:01:38 +0530

    types of classifiers

---
+++ Random Forest/RF1.R
@@ -0,0 +1,28 @@
+# RF is used for classification problem
+# It is a supervised learning model
+# spam classifier: what params does it look for?
+# subject line, text, html tags, ip addr of source etc.
+
+# Types of Classifiers: Decision Tree, Random Forests and Naive Bayes
+
+# RF is an ensemble classifier made from many DT models
+# ensemble methods use multiple ML algos to get better predictive perf
+
+# ENtire dataset is split into 3 subsets
+# each subset leads to a DT
+# we have 3 DT and each DT leads to a certain outcome
+# RF compiles results from all the DTS and then leads to a final outcome
+
+
+# Bayes: describes prob of event based on certain prior knowledge of conditions that might be related to that event
+
+# eg: cancer related to age.. age can be used to accurately assess prob of having cancer
+
+
+
+
+
+
+
+
+
